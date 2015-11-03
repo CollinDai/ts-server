7784a3887571b97087989c4ec3df0ec98d961565
Peike Dai <dpk1216@gmail.com>
2015-11-02 22:42:46 -0500

    add api controller to customize returning json

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,32 +6,26 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\View;
 
-class MovieController extends Controller
+class HomeController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-        // $bos = new BoxOfficeService();
-        // dd($bos->topTen());
-        // header('Content-Type: text/plain; charset=utf-8');
-        // $sub = new SubtitleService();
-        // $result = $sub->test();
-        // $omdb = new OmdbService();
-        // $omdbResp = $omdb->searchByTitle('martian');
-
-        // dd($omdbResp);
-        dd(Movie::all()
+        return response()->json(['name' => 'Abigail', 'state' => 'CA']);
+//        return "Home Controller!";
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -41,8 +35,8 @@ class MovieController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  Request  $request
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -53,7 +47,7 @@ class MovieController extends Controller
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show($id)
     {
@@ -64,7 +58,7 @@ class MovieController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function edit($id)
     {
@@ -74,9 +68,9 @@ class MovieController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, $id)
     {
@@ -87,7 +81,7 @@ class MovieController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {
