ca51718697fb6cfa317766cdeb00882a7c24ec1c
PeikeDai <dpk1216@gmail.com>
2015-11-19 13:18:37 -0500

    1)return imdb_id from topTen. 2>enable route for subtitle search

--- app/Services/Movie/SubtitleService.php
+++ app/Services/Movie/SubtitleService.php
@@ -6,30 +6,32 @@ class SubtitleService {
 	private $token;
 	private $xmlrpc_client;
 	const URL = 'http://api.opensubtitles.org/xml-rpc';
+
 	public function __construct() {
 		$this->xmlrpc_client = new XMLRPC_Client(self::URL, false);
 	}
+
 	public function login($username='', $password='', $language='', $useragent = 'OSTestUserAgent') {
 		if (Cache::has('OSToken')) {
 			$this->token = Cache::get('OSToken');
-			return;
-		}
-		$loginResp = $this->xmlrpc_client->call('LogIn', array($username, $password, $language, $useragent));
-		$this->token = $loginResp['token'];
-		Cache::put('OSToken', $this->token, 15);
+		} else {
+    		$loginResp = $this->xmlrpc_client->call('LogIn', array($username, $password, $language, $useragent));
+    		$this->token = $loginResp['token'];
+    		Cache::put('OSToken', $this->token, 15);
+        }
 	}
 	public function getToken(){return $this->token;}
 
-/**
- * Search subtitle from OpenSubtitle.com based on IMDb ID 
- * without leading "tt".
- * return download link
- * @param  integer $imdbId the integer following the leading character.
- * @return string         the zip download link
- */
-	public function searchSubtitle($imdbId) {
+    /**
+     * Search subtitle from OpenSubtitle.com 
+     * based on IMDb ID without leading "tt".
+     * @param  integer $imdbId the integer following the leading character.
+     * @param string $language split by comma if multiple
+     * @return string the zip download link
+     */
+	public function searchSubtitle($imdbId, $languages='eng') {
 		$imdbId = $this->validateIMDbID($imdbId);
-		$params = array(array('sublanguageid'=>'eng','imdbid'=>$imdbId));
+		$params = array(array('sublanguageid'=>$languages,'imdbid'=>$imdbId));
 		$resp = $this->xmlrpc_client->call('SearchSubtitles', array($this->token, $params));
 		return $resp;
 	}
